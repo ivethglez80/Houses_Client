72345802e833d75236579450c05a5adb22f03272
ivethG <ivethglez80@gmail.com>
2024-04-29 17:47:27 -0300

    working on routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
@@ -8,14 +9,17 @@ import { HomeComponent } from './home/home.component';
   standalone: true,
   imports: [
             RouterOutlet, 
+            RouterModule,
             HomeComponent],
             template: `
             <main>
+            <a [routerLink]="['/']">
               <header class="brand-name">
                 <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
               </header>
+              </a>
               <section class="content">
-                <app-home></app-home>
+              <router-outlet></router-outlet>
               </section>
             </main>
           `,
